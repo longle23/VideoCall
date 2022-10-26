75047b34223959d6fbeef08bea2a81792e5cf200
longle23 <91379062+longle23@users.noreply.github.com>
2022-10-26 23:05:24 +0700

    finish video call

--- src/screens/CallingScreen/index.js
+++ src/screens/CallingScreen/index.js
@@ -15,6 +15,8 @@ const permissions = [
 const CallingScreen = () => {
     const [permissionGrandted, setPermissionGranted] = useState(false);
     const [callStatus, setCallStatus] = useState('Initializing...');
+    const [localVideoStreamId, setLocalVideoStreamId] = useState('');
+    const [remoteVideoStreamId, setRemoteVideoStreamId] = useState('');
 
     const navigation = useNavigation();
     const route = useRoute()
@@ -24,6 +26,7 @@ const CallingScreen = () => {
     const voximplant = Voximplant.getInstance();
 
     const call = useRef(incomingCall);
+    const endpoint = useRef(null);
 
     const goBack = () => {
         navigation.pop()
@@ -33,10 +36,10 @@ const CallingScreen = () => {
         const getPermissions = async () => {
             const granted = await PermissionsAndroid.requestMultiple(permissions);
             const recordAudioGranted =
-                granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] = 'granted';
+                granted[PermissionsAndroid.PERMISSIONS.RECORD_AUDIO] === 'granted';
             const cameraGranted =
-                granted[PermissionsAndroid.PERMISSIONS.CAMERA] = 'granted';
-            if (!cameraGranted | !recordAudioGranted) {
+                granted[PermissionsAndroid.PERMISSIONS.CAMERA] === 'granted';
+            if (!cameraGranted || !recordAudioGranted) {
                 Alert.alert('Permissions not granted');
             } else {
                 setPermissionGranted(true);
@@ -67,6 +70,13 @@ const CallingScreen = () => {
             subscribeToCallEvents();
         }
 
+        const answerCall = async () => {
+            subscribeToCallEvents();
+            endpoint.current = call.current.getEndpoints()[0];
+            subscribeToEndpointEvent();
+            call.current.answer(callSettings);
+        };
+
         const subscribeToCallEvents = () => {
             // Call Failed
             call.current.on(Voximplant.CallEvents.Failed, callEvent => {
@@ -87,7 +97,24 @@ const CallingScreen = () => {
             call.current.on(Voximplant.CallEvents.Disconnected, callEvent => {
                 navigation.navigate('Contacts');
             });
+
+            call.current.on(Voximplant.CallEvents.LocalVideoStreamAdded, callEvent => {
+                setLocalVideoStreamId(callEvent.videoStream.id);
+            });
+
+            call.current.on(Voximplant.CallEvents.EndpointAdded, callEvent => {
+                endpoint.current = callEvent.endpoint;
+                subscribeToEndpointEvent();
+            });
         }
+
+        const subscribeToEndpointEvent = async () => {
+            endpoint.current.on(
+                Voximplant.EndpointEvents.RemoteVideoStreamAdded, endpointEvent => {
+                    setRemoteVideoStreamId(endpointEvent.videoStream.id);
+                },
+            );
+        };
 
         const showError = reason => {
             Alert.alert("Call failed", `Reason: ${reason}`, [
@@ -96,6 +123,12 @@ const CallingScreen = () => {
                     onPress: navigation.navigate('Contacts'),
                 },
             ]);
+        }
+
+        if (isIncomingCall) {
+            answerCall();
+        } else {
+            makeCall();
         }
 
         makeCall();
@@ -117,6 +150,14 @@ const CallingScreen = () => {
             <Pressable onPress={goBack} style={styles.backButton}>
                 <Ionicons name='chevron-back' size={25} color={'white'} />
             </Pressable>
+
+            <Voximplant.VideoView
+                videoStreamId={remoteVideoStreamId}
+                style={styles.remoteVideo} />
+
+            <Voximplant.VideoView
+                videoStreamId={localVideoStreamId}
+                style={styles.localVideo} />
 
             <View style={styles.cameraPreview}>
                 <Text style={styles.name}>{user?.user_display_name}</Text>
@@ -141,6 +182,24 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingTop: 10,
         paddingHorizontal: 10
+    },
+    localVideo: {
+        width: 100,
+        height: 150,
+        backgroundColor: '#ffff6e',
+        borderRadius: 10,
+        position: 'absolute',
+        right: 10,
+        top: 100,
+    },
+    remoteVideo: {
+        backgroundColor: '#7b4e80',
+        borderRadius: 10,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 100,
     },
     name: {
         fontSize: 30,
